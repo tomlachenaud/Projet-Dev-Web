7a61e78c96fa7cb2b788cb0c7a248f2102717b0c
gorosumo <105277659+gorosumo@users.noreply.github.com>
2024-03-27 13:59:48 +0100

    Update Inscription.php

--- Inscription.php
+++ Inscription.php
@@ -1,3 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Inscription</title>
+        <link rel="stylesheet" type="text/css" href="index.css">
+    </head>
+    <body>
+    <div class="page">
+
+<div class="top-section section">
+    <div class="logo">
+        <img src="CYTech.png"> <!--Permet d'afficher le logo du site-->
+    </div>
+    <div class="titre">
+        <h1>Société Play Masters</h1>
+    </div>
+    <div class="right-items">
+        <a href="connexion.html" class="link"><div class="connexion">Se connecter</div></a>
+        <a href="panier.php" class="link"><div class="panier">Panier</div></a>
+    </div>
+    <div class="menu1">
+        <a href="index.php" class="link"><div class="index">Accueil</div></a>
+        <a href="cartes.php" class="link"><div class="cartes">Cartes</div></a>
+        <a href="plateaux.php" class="link"><div class="plateaux">Plateaux</div></a>
+        <a href="cassesTetes.php" class="link"><div class="cassesTetes">Casses-têtes</div></a>
+        <a href="contact.html" class="link"><div class="contact">Contact</div></a>
+    </div>
+</div>
+
+<div class="bandeau-gauche section">
+    <div class="menu2">
+        <a href="index.php" class="link"><div class="index">Accueil</div></a>
+        <a href="cartes.php" class="link"><div class="cartes">Cartes</div></a>
+        <a href="plateaux.php" class="link"><div class="plateaux">Plateaux</div></a>
+        <a href="cassesTetes.php" class="link"><div class="cassesTetes">Casses-têtes</div></a>
+        <a href="contact.html" class="link"><div class="contact">Contact</div></a>
+    </div>
+</div>
+
+<div class="middle-section section">
+    <div class="titre1 titretexte1">
+        <!-- Formulaire HTML pour l'inscription -->
+        <form method="post" action="">
+            <label for="new_email">Adresse e-mail:</label>
+            <input type="email" id="new_email" name="new_email" required><br><br>
+            <label for="new_password">Mot de passe:</label>
+            <input type="password" id="new_password" name="new_password" required><br><br>
+            <input type="submit" value="S'inscrire">
+        </form>
+
+<div class="bottom-section section ">
+    <a href="plan.html" class="link"><div class="plan">Plan du site</div></a> <!--A completer avec le plan-->
+    <div class="mention">
+        <b>Mentions légales</b><br><br>Copyright Société Lafleur<br>Webmaster CY Tech
+
+    </div> <!--A completer avec les mentions-->
+    <a href="#" class="link"><div class="contact">Contact</div></a> <!--A completer avec les contacts-->
+</div>
+</div>
+    </body>
+</html>
+
 <?php
 // Vérification si le formulaire d'inscription a été soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -37,16 +99,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "Inscription réussie!";
 }
 ?>
-<!DOCTYPE html>
-<html>
-    <body>
-        <!-- Formulaire HTML pour l'inscription -->
-        <form method="post" action="">
-            <label for="new_email">Adresse e-mail:</label>
-            <input type="email" id="new_email" name="new_email" required><br><br>
-            <label for="new_password">Mot de passe:</label>
-            <input type="password" id="new_password" name="new_password" required><br><br>
-            <input type="submit" value="S'inscrire">
-        </form>
-    </body>
-</html>
