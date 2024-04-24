d2b3d3fa3e2926c4ab8ba6c0e1bda8aa31d945a8
LiliRoseGicquel <124388564+LiliRoseGicquel@users.noreply.github.com>
2024-04-24 21:24:24 +0200

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panier</title>
-    <link rel="stylesheet" type="text/css" href="index.css">
-    <link rel="stylesheet" type="text/css" href="inscription.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" type="text/css" href="css/inscription.css">
 </head>
 <body>
     <div class="page">
