7e3e12c8e9b07991d0a496d5bdf357484ea1cc23
LiliRoseGicquel <124388564+LiliRoseGicquel@users.noreply.github.com>
2024-04-24 21:22:01 +0200

    Update cassestetes.php

--- cassestetes.php
+++ cassestetes.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ma Page</title>
-    <link rel="stylesheet" type="text/css" href="index.css">
-    <link rel="stylesheet" type="text/css" href="jeux.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" type="text/css" href="css/jeux.css">
 </head>
 <body>
 <script>
