33da98eb8b7248a09788033609fc802a1bce030b
LiliRoseGicquel <124388564+LiliRoseGicquel@users.noreply.github.com>
2024-04-24 21:25:27 +0200

    Update panier.php

--- panier.php
+++ panier.php
@@ -16,8 +16,8 @@ else{
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panier</title>
-    <link rel="stylesheet" type="text/css" href="index.css">
-    <link rel="stylesheet" type="text/css" href="panier.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" type="text/css" href="css/panier.css">
 </head>
 <body>
     <div class="page">
